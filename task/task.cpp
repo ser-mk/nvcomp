27712f339bb93439dbe28b447c6bea217ccc39f3
ser-mk <tabtre@yandex.ru>
2022-05-29 01:08:50 +0300

    new cascade manager

--- task/task.cpp
+++ task/task.cpp
@@ -64,21 +64,92 @@ static void free_gpu_buffer(GPUbuffer& buf){
   buf.size = 0;
 }
 
+typedef struct
+{
+  uint16_t chunk_size;
+  nvcompType_t type;
+} OptionsHeader_t;
+
+struct TaskCascadedManager : CascadedManager {
+
+  const nvcompBatchedCascadedOpts_t& options;
+
+  TaskCascadedManager(
+      const nvcompBatchedCascadedOpts_t& options = nvcompBatchedCascadedDefaultOpts,
+      cudaStream_t user_stream = 0,
+      int device_id = 0):
+      CascadedManager(options, user_stream, device_id),
+      options(options)
+  {
+  }
+
+  virtual ~TaskCascadedManager()
+  {
+  }
+
+  static cudaError_t check_error() {
+    return nv_check_error_last_call_and_clear();
+  }
+
+  static const size_t _SIZE_OF_OPTIONS_HEADER = 3; // the type value(1) + the chunk size(2) = 3
+
+  size_t calc_size_out(const CompressionConfig & comp_config) const {
+    return comp_config.max_compressed_buffer_size + _SIZE_OF_OPTIONS_HEADER;
+  }
+
+  cudaError_t save_options_header(uint8_t* comp_buffer) const {
+    const OptionsHeader_t header_host = { static_cast<uint16_t>(0x7FFF | this->options.chunk_size),
+                                         this->options.type};
+    CUDA_CHECK(cudaMemcpy(comp_buffer, &header_host, sizeof(header_host), cudaMemcpyHostToDevice));
+    return cudaSuccess;
+  }
+
+  cudaError_t cascade_compress(
+      const uint8_t* decomp_buffer,
+      uint8_t* comp_buffer,
+      const CompressionConfig& comp_config) {
+
+    CUDA_CHECK(this->save_options_header(comp_buffer));
+    comp_buffer += _SIZE_OF_OPTIONS_HEADER;
+    CascadedManager::compress(decomp_buffer, comp_buffer, comp_config);
+    CUDA_CHECK(TaskCascadedManager::check_error());
+    return cudaSuccess;
+  }
+
+  virtual size_t get_compressed_output_size(uint8_t* comp_buffer)
+  {
+    return impl->get_compressed_output_size(comp_buffer + _SIZE_OF_OPTIONS_HEADER) + _SIZE_OF_OPTIONS_HEADER;
+  }
+
+  static cudaError_t parse_options_header(const uint8_t * comp_buffer,  OptionsHeader_t & header_host)
+  {
+    CUDA_CHECK(cudaMemcpy(&header_host, comp_buffer, sizeof(header_host), cudaMemcpyDeviceToHost));
+    return cudaSuccess;
+  }
+
+  cudaError_t cascade_decompress(uint8_t* decomp_buffer,
+                                 const uint8_t* comp_buffer,
+                                 const DecompressionConfig& decomp_config){
+    const uint8_t* comp_buffer_after_header = comp_buffer + _SIZE_OF_OPTIONS_HEADER;
+    CascadedManager::decompress(decomp_buffer, comp_buffer_after_header, decomp_config);
+    CUDA_CHECK(TaskCascadedManager::check_error());
+    return cudaSuccess;
+  }
+
+};
+
 static cudaError_t nv_compress(cudaStream_t & stream, const nvcompBatchedCascadedOpts_t & options, GPUbuffer & input_data, GPUbuffer & compress_data, size_t & comp_size){
-  CascadedManager manager{options, stream};
+  TaskCascadedManager manager{options, stream};
   CUDA_CHECK(nv_check_error_last_call_and_clear());
   auto comp_config = manager.configure_compression(input_data.size);
   CUDA_CHECK(nv_check_error_last_call_and_clear());
-  const size_t size_out = comp_config.max_compressed_buffer_size;
+
+  const size_t size_out = manager.calc_size_out(comp_config);
   CUDA_CHECK(check_then_relloc(compress_data, size_out));
 
-  manager.compress(input_data.ptr, compress_data.ptr, comp_config);
+  CUDA_CHECK(manager.cascade_compress(input_data.ptr, compress_data.ptr, comp_config));
   CUDA_CHECK(cudaStreamSynchronize(stream));
-  CUDA_CHECK(nv_check_error_last_call_and_clear());
-  const nvcompStatus_t status = *comp_config.get_status();
-  if(status != nvcompSuccess){
-    printf("cascade compress status: %d\n", status);
-  }
+
   comp_size = manager.get_compressed_output_size(compress_data.ptr);
   CUDA_CHECK(nv_check_error_last_call_and_clear());
 
@@ -110,7 +181,10 @@ cudaError_t max_compress(cudaStream_t & stream, INPUT_VECTOR_TYPE & input, GPUbu
             const nvcompBatchedCascadedOpts_t options = {chunk_size, _DATA_TYPE, rle, delta, static_cast<bool>(delta_mode), bp};
             printf("\n");
             print_options(options);
-            CUDA_CHECK(nv_compress(stream, options, tmp, compress_data, comp_size));
+            if(cudaSuccess != nv_compress(stream, options, tmp, compress_data, comp_size)){
+              printf("Pass");
+              continue;
+            }
             printf("compress size: %zu", comp_size);
             if(min_size <= comp_size)
               continue;
@@ -130,31 +204,26 @@ cudaError_t max_compress(cudaStream_t & stream, INPUT_VECTOR_TYPE & input, GPUbu
 }
 
 cudaError_t nv_decompress(cudaStream_t & stream, GPUbuffer & compress_data, GPUbuffer & decompress_data, size_t & output_size){
-  nvcompBatchedCascadedOpts_t options = nvcompBatchedCascadedDefaultOpts;
+
+  OptionsHeader_t header_host;
+  CUDA_CHECK(TaskCascadedManager::parse_options_header(compress_data.ptr, header_host));
   /*
-   * the decompression manager doesn't check the data type in the input stream
+   * the decompression manager doesn't check the data type and chunk_size in the input stream
    * https://github.com/NVIDIA/nvcomp/issues/63
    */
-  options.type = _DATA_TYPE;
+  const nvcompBatchedCascadedOpts_t options = {header_host.chunk_size, header_host.type, 0, 0, false, 0};
 
-  CascadedManager manager{options, stream};
+  TaskCascadedManager manager{options, stream};
   CUDA_CHECK(nv_check_error_last_call_and_clear());
   auto decomp_config = manager.configure_decompression(compress_data.ptr);
   CUDA_CHECK(nv_check_error_last_call_and_clear());
   CUDA_CHECK(check_then_relloc(decompress_data, decomp_config.decomp_data_size));
 
-  manager.decompress(
+  CUDA_CHECK(manager.cascade_decompress(
       decompress_data.ptr,
       compress_data.ptr,
-      decomp_config);
+      decomp_config));
   CUDA_CHECK(cudaStreamSynchronize(stream));
-  const nvcompStatus_t status = *decomp_config.get_status();
-  if(status != nvcompSuccess){
-    printf("cascade decompress status: %d\n", status);
-  }
-
-  CUDA_CHECK(nv_check_error_last_call_and_clear());
-
   output_size = decomp_config.decomp_data_size;
 
   return cudaSuccess;
